f59c863d3bdff6e76ff8eba522b1a52a02888bb2
temoteo <temoteoprimo@gmail.com>
2025-01-15 06:17:09 -0300

    ex42 Analisando triangulos 2.0

---
+++ aulas_e_exercícios/analisando_triangulos.py
@@ -0,0 +1,17 @@
+n1 = int(input('Primeiro segmento: '))
+n2 = int(input('Segundo segmento:: '))
+n3 = int(input('Terceiro segmento: '))
+
+maior = n1
+
+if n1 >= n2 + n3 or n2 >= n1 + n3 or n3 >= n1 + n2:
+    print('NÃO PODE FORMAR UM TRIÂNGULO')
+
+elif n1 != n2 and n1 == n3 or n1 != n3 and n1 == n2:
+    print('Triangulo isóceles')
+
+elif n1 == n2 == n3:
+    print('TRIANGULO EQUILÁTERO')
+
+else:
+    print('TRIANGULO ESCALENO')
