286b29304ba0a443b511f2041cff0dec757a92d8
sabiopescador <temoteoprimo@gmail.com>
2025-12-17 21:39:05 -0300

    contagem de pares

--- aulas_e_exercícios/ex047_contagem_de_pares.py
+++ aulas_e_exercícios/ex047_contagem_de_pares.py
@@ -1,6 +1,5 @@
 # todos os números pares em um intervalo de 1 a 50
 n = 2
-for i in range (n,51,2):
-    print(n,end=' ')
-    n+=2
-print('Acabou')
+for n in range (n,51,2):
+   print(n, end=' ')
+    
